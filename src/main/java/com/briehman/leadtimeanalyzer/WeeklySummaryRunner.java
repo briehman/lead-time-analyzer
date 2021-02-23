d9b783dc62c2fd9dbac6553e17706bbf08cd094a
Brian Riehman <briehman@gmail.com>
2021-02-22 20:18:11 -0600

    Make summary parameters configurable
    Makes the summary parameters configurable so the rolling period can be
    specified and the timezone uses the system default.

--- src/main/java/com/briehman/leadtimeanalyzer/WeeklySummaryRunner.java
+++ src/main/java/com/briehman/leadtimeanalyzer/WeeklySummaryRunner.java
@@ -8,22 +8,25 @@ import com.briehman.leadtimeanalyzer.service.DeliveryDateStrategy;
 import com.briehman.leadtimeanalyzer.service.MergeDataService;
 import com.briehman.leadtimeanalyzer.service.MergeDateStrategy;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @Profile("weekly-summary")
-public class WeeklySummaryRunner implements CommandLineRunner {
+public class WeeklySummaryRunner implements ApplicationRunner {
 
     private static final Logger LOG = LoggerFactory.getLogger(WeeklySummaryRunner.class);
-    private static final int ROLLING_DAYS = 28;
+    public static final String TIMEZONE = ZoneId.systemDefault().getId();
+    public static final int DEFAULT_ROLLING_PERIOD = 28;
 
     private final CodeRepositoryRepository codeRepositoryRepository;
     private final MergeDataService mergeDataService;
@@ -37,34 +40,36 @@ public class WeeklySummaryRunner implements CommandLineRunner {
 
     @Override
     @Transactional
-    public void run(String... args) throws Exception {
-        LOG.info("EXECUTING : command line runner");
-
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Must provide repo name to populate");
+    public void run(ApplicationArguments args) {
+        if (args.getNonOptionArgs().isEmpty()) {
+            throw new IllegalArgumentException("Must provide repo name");
         }
 
-        String repoName = args[0];
+        int rollingPeriod = args.containsOption("rollingPeriod")
+                ? Integer.parseInt(args.getOptionValues("rollingPeriod").get(0))
+                : DEFAULT_ROLLING_PERIOD;
+
+        String repoName = args.getNonOptionArgs().get(0);
 
         CodeRepository codeRepository = codeRepositoryRepository.findByName(repoName)
                 .orElseThrow(() -> new RuntimeException(
                         "Unable to find repository with name '" + repoName + "'"));
 
-        System.out.println(ROLLING_DAYS + " day rolling stats window ending on below dates");
-        printStats(codeRepository, LocalDate.now());
-        printStats(codeRepository, LocalDate.now().minus(2, ChronoUnit.WEEKS));
-        printStats(codeRepository, LocalDate.now().minus(3, ChronoUnit.MONTHS));
-        printStats(codeRepository, LocalDate.now().minus(6, ChronoUnit.MONTHS));
-        printStats(codeRepository, LocalDate.now().minus(1, ChronoUnit.YEARS));
+        System.out.println(rollingPeriod + " day rolling stats window ending on below dates. All values in days.");
+        printStats(codeRepository, LocalDate.now(), rollingPeriod);
+        printStats(codeRepository, LocalDate.now().minus(2, ChronoUnit.WEEKS), rollingPeriod);
+        printStats(codeRepository, LocalDate.now().minus(3, ChronoUnit.MONTHS), rollingPeriod);
+        printStats(codeRepository, LocalDate.now().minus(6, ChronoUnit.MONTHS), rollingPeriod);
+        printStats(codeRepository, LocalDate.now().minus(1, ChronoUnit.YEARS), rollingPeriod);
     }
 
-    private void printStats(CodeRepository codeRepository, LocalDate endingOn) {
+    private void printStats(CodeRepository codeRepository, LocalDate endingOn, int rollingPeriod) {
         DeliveryDateStrategy deliveryDateStrategy = new MergeDateStrategy();
-        LocalDate twoWeeksAgo = endingOn.minus(ROLLING_DAYS, ChronoUnit.DAYS);
+        LocalDate twoWeeksAgo = endingOn.minus(rollingPeriod, ChronoUnit.DAYS);
 
         PeriodDto leadTimeData = mergeDataService
-                .getMergeData(twoWeeksAgo, endingOn, "America/Chicago", null, deliveryDateStrategy,
-                        codeRepository, null, true, true, ROLLING_DAYS);
+                .getMergeData(twoWeeksAgo, endingOn, TIMEZONE, null, deliveryDateStrategy,
+                        codeRepository, null, true, true, rollingPeriod);
         List<DeliveryDateDto> data = leadTimeData.getData();
         DeliveryDateDto newestData = data.get(data.size() - 1);
         System.out.println(newestData.getDate() + ": " + newestData.getRollingStats().statusString());
